71535dbda61b5a9581428864ff8bdcb98deb7d0f
Abraham_S <abraham7dsm@gmail.com>
2023-12-26 11:05:25 -0400

    restructure update service

--- api/services/updateBalances.js
+++ api/services/updateBalances.js
@@ -1,9 +1,9 @@
+import { _balance } from "./balance.js"
+
 export const _updateBalances = async ({ MARKET1, MARKET2, store }) => {
     const balance = await _balance()
-    const key1 = MARKET1.toLowerCase() + '_balance'
-    const value1 = fl(balance[MARKET1].available)
-    const key2 = MARKET2.toLowerCase() + '_balance'
-    const value2 = fl(balance[MARKET2].available)
-    store.put(key1, value1)
-    store.put(key2, value2)
+    const key = market => market.toLowerCase() + '_balance'
+    const value = market => fl(balance[market].available)
+    store.put(key(MARKET1), value(MARKET1))
+    store.put(key(MARKET2), value(MARKET2))
 }
